04beafa7b2efbab524e3769ff8cdedc58c481a73
Jon <jonalmazan@ucode.com>
2015-05-23 01:23:50 -0700

    Finished socket.io chat. Server emits the event to rest of users and outputs messages on client side

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+/*Socket.IO is composed of two parts:
+    A server that integrates with (or mounts on) the Node.JS HTTP Server: socket.io
+    A client library that loads on the browser side: socket.io-client
+*/
+
+
 // Express initializes app to be a function handler that you can supply to an HTTP server 
 var app = require('express')(); 
+// HTTP server
 var http = require('http').Server(app);
 //initialize a new instance of socket.io by passing the http (the HTTP server) object. 
 var io = require('socket.io')(http);
@@ -25,7 +32,13 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-  
+
+  // print out the chat message event
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    //send the message to everyone, including the sender.
+    io.emit('chat message', msg);
+  });
 });
 
 //We make the http server listen on port 3000.
@@ -33,3 +46,14 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
+
+/*
+In order to send an event to everyone, Socket.IO gives us the io.emit:
+	io.emit('some event', { for: 'everyone' });
+*/
+
+/* send a message to everyone except for a certain socket, we have the broadcast flag:
+io.on('connection', function(socket){
+  socket.broadcast.emit('hi');
+});
+*/
